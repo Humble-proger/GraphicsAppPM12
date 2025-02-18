7d7d08805f3879357ae9709fd835a228769f237b
Александра Быковская <Aibykovs@mail.ru>
2025-02-18 15:44:19 +0700

    added point and line class

---
+++ Geometry/Point.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+
+public class Point : Avalonia.Controls.Shapes.Ellipse
+{
+    public Point(double x, double y, double thickness = 1, string color = "#000000")
+    {
+        Width = thickness;
+        Height = thickness;
+        Fill = new SolidColorBrush(Color.Parse(color));
+        Canvas.SetLeft(this, x);
+        Canvas.SetTop(this, y);
+    }
+}
+
