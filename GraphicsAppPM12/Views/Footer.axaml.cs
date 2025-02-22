db1812858b234644ce87280c74816668aa2c2fee
Marina <pm11.belmesova@gmail.com>
2025-02-23 00:44:18 +0700

    создала футер
    без кнопок и всего остального
    только файлы

---
+++ GraphicsAppPM12/Views/Footer.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GraphicsApp.Views
+{
+    public partial class Footer : UserControl
+    {
+        public Footer()
+        {
+            InitializeComponent();
+        }
+    }
+}
