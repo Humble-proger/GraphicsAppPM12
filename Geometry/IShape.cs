f6e2a31405b37b2467a5e78d18119d65815c69fd
Александра Быковская <Aibykovs@mail.ru>
2025-02-22 21:22:09 +0700

    Interfaces for shape and tranformation, implementation of transformation

---
+++ Geometry/IShape.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Media;
+
+// интерфейс всех фигур
+public interface IShape
+{
+    // координата х центра фигуры
+    double CenterX { get; set; }
+    // координата y центра фигуры
+    double CenterY { get; set; }
+    // толщина обводки)
+    double StrokeThickness { get; set; }
+    // цвет обводки
+    IBrush Stroke { get; set; }
+    // цвет заливки
+    IBrush Fill { get; set; }
+    // Метод для смены цвета обводки
+    void SetStroke(Color color);
+    // Метод для смены цвета
+    void SetColor(Color color);
+    // Метод для смены толщины контура
+    void SetThickness(double thickness);
+}
---
+++ Geometry/IShapeOfPoints.cs
@@ -0,0 +1,11 @@
+﻿// интерфейс фигур, строящихся по точкам
+interface IShapeOfPoints : IShape
+{
+    // список точек составляющих фигуру
+    List<Avalonia.Point> Points { get; set; }
+
+    // метод добавления новых точек
+    void addPoint(Avalonia.Point point);
+    // метод удаления точек
+    void deletePoint(Avalonia.Point point);
+}
