c38905f59ef401f343f7cfbb86b8883f7afbd775
Marina <pm11.belmesova@gmail.com>
2025-02-23 02:22:11 +0700

    зум
    сделала в футере зум, даже кнопки работают)

--- GraphicsAppPM12/Views/Footer.axaml.cs
+++ GraphicsAppPM12/Views/Footer.axaml.cs
@@ -1,12 +1,33 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace GraphicsApp.Views
 {
     public partial class Footer : UserControl
     {
+        private int _zoomLevel = 100;
+
         public Footer()
         {
             InitializeComponent();
+        }
+        
+        private void OnZoomInClicked(object sender, RoutedEventArgs e)
+        {
+            if (_zoomLevel < 500) // Ограничиваем увеличение до 500%
+            {
+                _zoomLevel += 10;
+                ZoomText.Text = $"{_zoomLevel}%";
+            }
+        }
+
+        private void OnZoomOutClicked(object sender, RoutedEventArgs e)
+        {
+            if (_zoomLevel > 10) // Ограничиваем уменьшение до 10%
+            {
+                _zoomLevel -= 10;
+                ZoomText.Text = $"{_zoomLevel}%";
+            }
         }
     }
 }
