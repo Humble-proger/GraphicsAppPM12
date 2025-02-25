f11c91dffb2c15f047df77065935820f6f221c94
Marina <pm11.belmesova@gmail.com>
2025-02-25 20:43:30 +0700

    изменила панель фигур
    сделано не до конца:
    надо вставить иконки, а не текст
    привести к нормальному виду

--- GraphicsAppPM12/Views/ToolBars.axaml.cs
+++ GraphicsAppPM12/Views/ToolBars.axaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Avalonia.Controls;
 
 namespace GraphicsApp.Views
 {
@@ -7,6 +11,38 @@ namespace GraphicsApp.Views
         public ToolBars()
         {
             InitializeComponent();
+             
+            // Создаем список фигур и сортируем его по имени
+            var shapes = new List<ShapeItem>
+            {
+                 new ShapeItem("Линия", "pictures/ToolBars/line.png"), 
+                 new ShapeItem("Кривая", "pictures/ToolBars/polyline.png"),
+                 new ShapeItem("Квадрат", "pictures/ToolBars/square.png"),
+                 new ShapeItem("Треугольник", "pictures/ToolBars/triangle.png"), 
+                 new ShapeItem("Круг", "pictures/ToolBars/circle.png")
+                 
+             };
+            
+            // Устанавливаем DataContext для UserControl
+            this.DataContext = this;
+            
+            // Устанавливаем ItemsSource для ListBox
+            shapesList.ItemsSource = shapes;
         }
     }
+
+    public class ShapeItem
+    {
+        public string Name { get; }
+        public string IconPath { get; set; }
+
+        public ShapeItem(string name, string iconPath)
+        {
+            Name = name;
+            IconPath = iconPath;
+
+        }
+    }
+
+    
 }
