b50a83114e7313afa40854f132f00b622a7080ee
AndyCale <57535454+AndyCale@users.noreply.github.com>
2025-02-28 10:26:03 +0700

    added name parameter to the shape, changed transformations.Fixed the movement (#17)

--- Geometry/IAffineTranformable.cs
+++ Geometry/IAffineTranformable.cs
@@ -1,14 +1,12 @@
-﻿using Avalonia.Controls.Shapes;
-
-// интерфейс преобразования фигур
+﻿// интерфейс преобразования фигур
 interface IAffineTransformable
 {
     // перемещение объекта, (x, y) - координаты его верхней левой точки
-    void move(Shape shape, double deltaX, double deltaY);
+    void move(IShape shape, double deltaX, double deltaY);
     // поворот фигуры относительно центра, degree - угол поворота
-    void rotate(Shape shape, double angle, double x, double y);
+    void rotate(IShape shape, double angle, double x, double y);
     // отзеркаливание относительно точки
-    public void reflection(Shape shape, double x, double y);
+    public void reflection(IShape shape, double x, double y);
     // изменение размеров фигуры, х и у - какие параметры ширины и высоты установить
-    void scale(Shape shape, double scaleX, double scaleY, double x, double y);
+    void scale(IShape shape, double scaleX, double scaleY, double x, double y);
 }
