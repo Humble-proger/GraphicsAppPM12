1bfb1643501fd8ace16374a2cd83a77e5b31b4a8
KirillVilkin <forkin.kir@yandex.ru>
2025-03-05 15:13:55 +0700

    Не добавил файл, в котором реализация методов для Footer.

--- GraphicsAppPM12/ViewModels/MainWindowViewModel.cs
+++ GraphicsAppPM12/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,45 @@
-﻿using static GraphicsApp.Views.Layers;
-using System.Collections.ObjectModel;
+﻿using System.Numerics;
+using System.Windows.Input;
 
 namespace GraphicsApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    //public string Greeting { get; } = "Здравствуйте, тимлидер Дмитрий!";
+    private string _mouseCoord = "X: 0.0 Y: 0.0";
+    private string _canvasSize = "0 × 0";
     
+    public string MouseCoords {
+        get => _mouseCoord;
+        set {
+            _mouseCoord = value;
+            OnPropertyChanged(nameof(MouseCoords));
+        }
+    }
+    public string CanvasSize {
+        get => _canvasSize;
+        set {
+            _canvasSize = value;
+            OnPropertyChanged(nameof(CanvasSize));
+        }
+    }
+    public ICommand ChangeMouseCoord { get; }
+    public ICommand ChangeSizeCanvas { get; }
+
+    public MainWindowViewModel()
+    {
+        ChangeMouseCoord = new RelayCommand(OnMouseMove);
+        ChangeSizeCanvas = new RelayCommand(OnResizeCanvas);
+    }
+    
+    private void OnMouseMove(object point) {
+        if (point is Avalonia.Point _point) {
+            MouseCoords = $"X: {_point.X} Y: {_point.Y}";
+        }
+    }
+    private void OnResizeCanvas(object size) 
+    {
+        if (size is Vector2 _size) {
+            CanvasSize = $"{_size.X} × {_size.Y} мм";
+        }
+    }
 }
