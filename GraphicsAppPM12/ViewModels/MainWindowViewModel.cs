9c762b7b7cb988511897ebb7a934dbff1f1e974d
KirillVilkin <forkin.kir@yandex.ru>
2025-03-06 11:45:51 +0700

    Footer (#24)
    * Реализовано отображение координат курсора на экране.
    
    * Не добавил файл, в котором реализация методов для Footer.
    
    ---------
    
    Co-authored-by: Humble-proger <85777203+Humble-proger@users.noreply.github.com>

--- GraphicsAppPM12/ViewModels/MainWindowViewModel.cs
+++ GraphicsAppPM12/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+using System.Numerics;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
 using System.Composition;
 using System.Composition.Hosting;
 using System.Windows.Input;
@@ -14,7 +16,9 @@ namespace GraphicsApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    //public string Greeting { get; } = "Здравствуйте, тимлидер Дмитрий!";
+    private string _mouseCoord = "X: 0.0 Y: 0.0";
+    private string _canvasSize = "0 × 0";
+    
     public ObservableCollection<ShapeViewModel> Figures { get; } = [];
 
     public ObservableCollection<ModelFactoryViewModel> Factories { get; } = [];
@@ -24,6 +28,23 @@ public partial class MainWindowViewModel : ViewModelBase
 
     [ImportMany]
     private IEnumerable<ExportFactory<IShape>> ModelFactories { get; set; } = [];
+    
+    public string MouseCoords {
+        get => _mouseCoord;
+        set {
+            _mouseCoord = value;
+            OnPropertyChanged(nameof(MouseCoords));
+        }
+    }
+    public string CanvasSize {
+        get => _canvasSize;
+        set {
+            _canvasSize = value;
+            OnPropertyChanged(nameof(CanvasSize));
+        }
+    }
+    public ICommand ChangeMouseCoord { get; }
+    public ICommand ChangeSizeCanvas { get; }
 
     public MainWindowViewModel()
     {
@@ -36,6 +57,8 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             Factories.Add(new() { Factory = factory, Main = this });
         }
+        ChangeMouseCoord = new RelayCommand(OnMouseMove);
+        ChangeSizeCanvas = new RelayCommand(OnResizeCanvas);
     }
 
     private void LoadFigures(IEnumerable<ShapeViewModel>? figures)
@@ -48,6 +71,17 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             fig.Main = this;
             Figures.Add(fig);
+        }
+    }
+    private void OnMouseMove(object point) {
+        if (point is Avalonia.Point _point) {
+            MouseCoords = $"X: {_point.X} Y: {_point.Y}";
+        }
+    }
+    private void OnResizeCanvas(object size) 
+    {
+        if (size is Vector2 _size) {
+            CanvasSize = $"{_size.X} × {_size.Y} мм";
         }
     }
 }
---
+++ GraphicsAppPM12/Views/MyCanvas.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using System.Numerics;
+using GraphicsApp.ViewModels;
+
+namespace GraphicsApp.Views
+{
+    public partial class MyCanvas : UserControl
+    {
+        public MyCanvas()
+        {
+            InitializeComponent();
+        }
+        
+        private void MouseMoved(object sender, PointerEventArgs args) 
+        {
+            if (DataContext is MainWindowViewModel viewModel) {
+                var point = args.GetPosition((Canvas) sender);
+                viewModel.ChangeMouseCoord.Execute(point);
+            }
+        }
+        
+        private void CanvasResize(object sender, SizeChangedEventArgs e) 
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+            {
+                Vector2 newSize = new Vector2() { X = (float)e.NewSize.Width, Y = (float)e.NewSize.Height };
+
+                viewModel.ChangeSizeCanvas.Execute(newSize);
+            }
+        }
+    }
+}
