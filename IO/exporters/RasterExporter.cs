be50d92cdda0a2f3de00f9f8d1b0aa2a448237ea
Humble-proger <85777203+Humble-proger@users.noreply.github.com>
2025-03-07 11:21:30 +0700

    Добавление растрового экспортера и Json экспортера и импортера (#28)
    * Сделан растровый экспортер
    
    App.cs в корне проекта необходим для тестового канваса. Сделал 'ручной' подсчет ширины и высоты для тестового канваса. Убрал лишний интерфейс и класс (IShapeData, BaseExporter).
    
    * добавил сериализатор и его реализацию (#22)
    
    Co-authored-by: Ilya M <laurentnotsaint@gmail.com>
    
    ---------
    
    Co-authored-by: Vlad <vldstkn.develop@gmail.com>
    Co-authored-by: ilyamoussatkine <eliasmousatkine@gmail.com>
    Co-authored-by: Ilya M <laurentnotsaint@gmail.com>

---
+++ IO/exporters/RasterExporter.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+
+using IO.interfaces;
+
+namespace IO.exporters;
+
+public class RasterExporter: IExporter
+{
+    public void Export(Canvas canvas, string filepath)
+    {
+        if (canvas.Bounds.Width == 0 || canvas.Bounds.Height == 0)
+        {
+            throw new InvalidOperationException("The canvas does not have the correct dimensions. Make sure it is rendered.");
+        }
+        var pixelSize = new PixelSize((int)canvas.Bounds.Width, (int)canvas.Bounds.Height);
+        var dpi = new Vector(96, 96);
+        var renderBitmap = new RenderTargetBitmap(pixelSize, dpi);
+        renderBitmap.Render(canvas);
+        using (var stream = File.OpenWrite(filepath))
+        {
+            renderBitmap.Save(stream);
+        }
+    }
+}
