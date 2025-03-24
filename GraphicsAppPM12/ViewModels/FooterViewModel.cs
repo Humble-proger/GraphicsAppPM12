8532f5a4293dbf285422b0dffeafaea168b4f499
ilyamoussatkine <eliasmousatkine@gmail.com>
2025-03-24 20:30:26 +0700

    Изменения в JsonSerializer; Изменения в Footer

--- GraphicsAppPM12/ViewModels/FooterViewModel.cs
+++ GraphicsAppPM12/ViewModels/FooterViewModel.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace GraphicsApp.ViewModels
 {
@@ -19,11 +21,19 @@ namespace GraphicsApp.ViewModels
         [ObservableProperty]
         private string _canvasSize;
 
+        [ObservableProperty]
+        private string _zoomString = "100%";
+
+        public ICommand ZoomIn { get; }
+        public ICommand ZoomOut { get; }
+
         public FooterViewModel(MainWindowViewModel? main)
         {
             Main = main;
             MouseCoords = "";
             CanvasSize = "";
+            ZoomIn = new RelayCommand(() => Main?.Canvasview.Zoom(0.1));
+            ZoomOut = new RelayCommand(() => Main?.Canvasview.Zoom(-0.1));
         }
     }
 }
--- GraphicsAppPM12/Views/Footer.axaml.cs
+++ GraphicsAppPM12/Views/Footer.axaml.cs
@@ -1,38 +1,13 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-
-using GraphicsApp.ViewModels;
 
 namespace GraphicsApp.Views
 {
     public partial class Footer : UserControl
     {
-        private int _zoomLevel = 100;
-
         public Footer()
         {
             InitializeComponent();
         }
         
-        private void OnZoomInClicked(object sender, RoutedEventArgs e)
-        {
-            if (_zoomLevel < 500 && DataContext is FooterViewModel viewModel) // Ограничиваем увеличение до 500%
-            {
-                _zoomLevel += 10;
-                ZoomText.Text = $"{_zoomLevel}%";
-                viewModel.Main?.Canvasview.Zoom(0.1);
-
-            }
-        }
-
-        private void OnZoomOutClicked(object sender, RoutedEventArgs e)
-        {
-            if (_zoomLevel > 10 && DataContext is FooterViewModel viewModel) // Ограничиваем уменьшение до 10%
-            {
-                _zoomLevel -= 10;
-                ZoomText.Text = $"{_zoomLevel}%";
-                viewModel.Main?.Canvasview.Zoom(-0.1);
-            }
-        }
     }
 }
